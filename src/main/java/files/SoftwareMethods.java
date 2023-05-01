b990c745906b285d501392649e9d6a95556cf8e5
Carlos Lopes ✔ <lopes.carlos.host@gmail.com>
2023-05-01 11:34:48 -0300

    (feat): implemented hashmaps
    These hashmaps will care about the subdirectories

--- src/main/java/files/SoftwareMethods.java
+++ src/main/java/files/SoftwareMethods.java
@@ -1,8 +1,11 @@
 package files;
+import java.util.HashMap;
 
 public class SoftwareMethods {
-  String[] codeClassNames = {"ExemploInput.java", "Introduction.java", "Loops.java", "PrimitivoVsObjeto.java", "OrderApp.java"};
+  String[] codeClassNames = {"ExemploInput.java", "Introduction.java", "Loops.java", "PrimitivoVsObjeto.java", "OrderApp.java", "BankAccountApp.java"};
   String[] codeExercisesNames = {"Apple.java", "Wage.java", "Biggestandsmallest.java", "AlunoApp.java", "Imc.java"};
+
+  HashMap<String, String> subDirectoriesClass = new HashMap<>();
   
   public void showStartMenu() {
     System.out.println("\n+-------------------------------------+");
@@ -39,21 +42,33 @@ public class SoftwareMethods {
     }
   }
 
-  public void showCodeInformation(String code, int menuType) {
+  public void showCodeInformation(String codeName, int menuType) {
+    String codeDirectory = codeName;
+    subDirectoriesClass.put("AlunoApp.java", "MediaAluno/");
+    subDirectoriesClass.put("BankAccountApp.java", "Bank/");
+    subDirectoriesClass.put("ExemploInput.java", "InputExample/");
+    subDirectoriesClass.put("OrderApp.java", "Orders/");
+
+    if (subDirectoriesClass.containsKey(codeName)) {
+      codeDirectory = subDirectoriesClass.get(codeName) + codeName;
+    }
+
     switch (menuType) {
       case 0:
-        System.out.println("\n\nInicializando " + code + " ...");
-        System.out.printf("Código fonte em: https://github.com/LopesLs/JavaLearn/blob/main/src/AulasCode/%s\n\n", code);
+        System.out.println("\nInicializando " + codeName + " ...");
+        System.out.printf("Código fonte em: https://github.com/LopesLs/JavaLearn/blob/main/src/main/java/files/AulasCode/%s\n", codeDirectory);
         break;
       
       case 1:
-        System.out.println("\n\nInicializando " + code + " ...");
-        System.out.printf("Código fonte em: https://github.com/LopesLs/JavaLearn/blob/main/src/ExercisesCode/%s\n\n", code);
+        System.out.println("\nInicializando " + codeName + " ...");
+        System.out.printf("Código fonte em: https://github.com/LopesLs/JavaLearn/blob/main/src/main/java/files/ExercisesCode/%s\n", codeDirectory);
         break;
       
       default:
-        System.out.println("\n\nOpção inválida!");
+        System.out.println("\nOpção inválida!");
         break;
     }
+
+    subDirectoriesClass.clear();
   }
 }
