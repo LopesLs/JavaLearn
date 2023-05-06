a92b0bfa36b09b9a445254e9dfccb1b3ba501fbd
Carlos Lopes ✔ <lopes.carlos.host@gmail.com>
2023-05-06 11:29:00 -0300

    (Feat): AddsPlayerApp class with player functionality
    Adds the PlayerApp class with the implementation of player functionality, such as adding goals and assists, checking the number of goals and assists, and creating a player. The class uses a HashMap to store player statistics. The program is interactive and displays a menu with options for the user. The code is in the PlayerApp.java file inside the src/main/java/files/ExercisesCode directory.

---
+++ src/main/java/files/ExercisesCode/PlayerApp.java
@@ -0,0 +1,88 @@
+// Criar uma classe Jogador que tenha as propriedades nome, gols, assistências. E os métodos adicionarGol e adicionarAssistencia que receba como argumento os quantidade de gols/assistências. Também criar dois métodos para ver o número de gols e assistências
+
+package files.ExercisesCode;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+
+public class PlayerApp {
+  public static void main(String[] args) {
+    HashMap<String, ArrayList<Integer>> players = new HashMap<String, ArrayList<Integer>>();
+    Player player = new Player(null, null);
+    Scanner scanner = new Scanner(System.in);
+
+    // Introduzindo o programa para o usuário
+    System.out.println("\n> Player.java");
+    System.out.println("Este programa cria um jogador e adiciona gols e assistências a ele.");
+
+    while (true) {
+      System.out.println(players);
+
+      System.out.println("\nOpções:");
+      System.out.println("1 - Criar jogador");
+      System.out.println("2 - Adicionar gols");
+      System.out.println("3 - Adicionar assistências");
+      System.out.println("4 - Ver gols");
+      System.out.println("5 - Ver assistências");
+      System.out.println("0 - Sair\n");
+
+      System.out.print("Digite a opção desejada: ");
+      int option = scanner.nextInt();
+
+      switch (option) {
+        case 0:
+          System.out.println("\nSaindo...");
+          scanner.close();
+          System.exit(option);
+          break;
+
+        case 1:
+          System.out.print("\nDigite o nome do jogador: ");
+          player.nome = scanner.next();
+
+          ArrayList<Integer> playerStatistics = new ArrayList<Integer>();        
+          playerStatistics.add(0);
+          playerStatistics.add(0);
+
+          player.statistics = playerStatistics;
+          players.put(player.nome, player.statistics);
+  
+          System.out.printf("\nJogador %s criado com sucesso!\n", player.nome);
+          break;
+
+        case 2:
+          System.out.print("\nDigite o nome do jogador: ");
+          player.nome = scanner.next();
+
+          player.setGol(players, scanner);
+          
+          break;
+        
+        case 3:
+          System.out.print("\nDigite o nome do jogador: ");
+          player.nome = scanner.next();
+
+          player.setAssistence(players, scanner);
+
+          break;
+        
+        case 4:
+          System.out.print("\nDigite o nome do jogador: ");
+          player.nome = scanner.next();
+
+          System.out.printf("\n%s\n", player.getGols(players, player.nome));
+
+          break;
+        
+        case 5:
+          System.out.print("\nDigite o nome do jogador: ");
+          player.nome = scanner.next();
+
+          System.out.printf("\n%s\n", player.getAssistencia(players, player.nome));
+
+          break;
+      }
+    }
+  }
+}
